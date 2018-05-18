2e731340556c5703812d8117356b8c7972161afb
OvidijusKutkaitis <o.kutkaitis@gmail.com>
2018-05-18 16:41:58 +0300

    Penktadienis !!!

--- dirbame_cia/diena_5/Ovidijus/1uzduotis/main.js
+++ dirbame_cia/diena_5/Ovidijus/1uzduotis/main.js
@@ -42,9 +42,9 @@ console.log(typeof(atlyginimas));
  // kuri atspausdina i konsole pirmos uzduoties kintamuosius
 
 function printVardasPavardeAmzius() {
-  console.log(vardas);
-  console.log(pavarde);
-  console.log(amzius);
+  console.log("Vardas: ",vardas);
+  console.log("Pavarde: ",pavarde);
+  console.log("Amzius: ",amzius);
 }
 printVardasPavardeAmzius();
 
@@ -54,7 +54,43 @@ printVardasPavardeAmzius();
 // (vienas atlyginimas yra lygus "uzduotis 1" kintamajam - "atlyginimas")
 
 function printMetinisPajamuDydis() {
-  pajamos = atlyginimas * 12;
-  console.log(pajamos);
+var pajamos = atlyginimas * 12;
+  console.log("Metines pajamos: ",pajamos);
 }
 printMetinisPajamuDydis();
+
+//  4 UZDUOTIS
+// A) sukurti kintamuosius:  salis, miestas, adresas, pastoKodas
+// B) sukurti f-ja "printAddressData()",  kuri atspausdina i konsole visus siuos kintamuosius
+
+var salis = "Lietuva";
+var miestas = "Kaunas";
+var adresas = "Kursiu";
+var pastoKodas = 33268;
+
+function printAddressData(){
+  console.log(salis);
+  console.log(miestas);
+  console.log(adresas);
+  console.log(pastoKodas);
+}
+printAddressData();
+
+//  5 UZDUOTIS
+// sukurti funkcija "printTekstas(x)" ,
+//  kuri atspausdina i konsole "x" reiksmes
+// iskviesti f-ja ir vietoj x irasyti koki nors teksta pvz: printTekstas( "Jokubo istorijos");
+
+function printTekstas(x){
+  console.log(x);
+}
+printTekstas("Jokubo istorijos");
+
+// 6 UZDUOTIS
+// sukurti f-ja, kuri sudaugina du  paduotus skaicius "daugyba(x, y)"
+
+function daugyba(x,y) {
+var suma = x*y;
+console.log("Sudauginus", x,y,"gauname",suma);
+}
+daugyba(100,5);
