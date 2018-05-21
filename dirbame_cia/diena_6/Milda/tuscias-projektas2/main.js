eca336f726a7a7da35e322ff0977d31627fffaac
Lilija272 <neverenda@gmail.com>
2018-05-21 13:36:46 +0300

    no message

--- dirbame_cia/diena_6/Milda/tuscias-projektas2/main.js
+++ dirbame_cia/diena_6/Milda/tuscias-projektas2/main.js
@@ -1,30 +1,3 @@
-// ==============patarimai==========
-// reiksme  - kintamasis  / variable
-// "reiksme" - tekstas/ reiksme/ value
-// reiksme() - f-jos paleidimas
-// =============================
-
-
-
-//======================TEKSTO ISVEDIMAS (tik susipazinti)===============
-// UZDUOTIS
-// Issimeginti komandas:
-
-// document.write(" <b class='bg-info'> Mano tekstas </b> ");
-// window.alert("labas");
-// alert("Viso");
-// var ivestasTekstas = prompt("Ivestkite savo varda");
-// console.log(ivestasTekstas);
-// document.querySelector('h1').innerHTML = " <b>Antraste</b>";
-
-//----Elemento duomenys--------
-// auksto paemimas:
-// var aukstis = document.getElementById('manoDiv').clientHeight;   // clientHeight includes the height and vertical padding.
-// var aukstis = document.getElementById('manoDiv').offsetHeight;  //  offsetHeight includes the height, vertical padding, and vertical borders.
-// aukscio nustatymas:
-// document.getElementById("manoDiv").style.height = "500px"; 		// change the height of a <div> element:
-
-//=====================PASIKARTOJIMAS==================
 
 //  0 UZDUOTIS
 //  sukurti funkcija "printKaina(x)" ,
@@ -32,14 +5,49 @@
 //  iskviesti f-ja 3 kartus, kad atspausdintu 3 skirtingas kainas:  999.00 , 13.49, 100.05
 //  ir vietoj x irasyti, koki nors teksta pvz: printKaina( 999.00);
 
+function printKaina (kaina) {
+  console.log(kaina);
+
+}
+printKaina (999.00);
+printKaina (13.49);
+printKaina (100.05);
+
 // 1 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis()", kuri apskaiciuoja vidurki is  5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki (pazymius sukurti f-jos viduje) 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
+
+function pazymiuVidurkis0 (x1, x2, x3, x4, x5) {
+  console.log( "vidurkis5" + (x1 + x2 + x3 + x4 + x5) / 5);
+}
+pazymiuVidurkis0  (5, 10, 8, 6, 8);
+
+
+// arba 2 budas kaip spresti
+function pazymiuVidurkis2() {
+  var x1 = 5;
+  var x2 = 10;
+  var x3 = 8;
+  var x4 = 6;
+  var x5 = 8;
+  var vidurkis = (x1 + x2 + x3 + x4 + x5)/5;
+  console.log("vidurkis1" + vidurkis);
+}
 
 // 1.1 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis1(x1, x2, x3, x4, x5)", kuriai galima paduoti 5 kintamuosius,
 // kuri apskaiciuoja vidurki is paduotu 5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki, 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
+
+function pazymiuVidurkis1 (x1, x2, x3, x4, x5, vardas) {
+var vidurkis1 = (x1 + x2 + x3 + x4 + x5)/5;
+document.querySelector('h1').innerHTML += vidurkis1 + vardas;
+}
+
+pazymiuVidurkis1 (1, 9, 9, 8, 9, "petras");
+pazymiuVidurkis1 (8, 9, 9, 8, 10, "marius");
+pazymiuVidurkis1 (3, 4, 5, 5, 7, "juozas");
+
 
 
 //=====================RETURN==================
@@ -48,11 +56,31 @@
 // parasyti f-ja "getVardas()", kuri turi "return" zodeli ir grazina varda i iskveitimo vieta.
 // sukurti kintamaji "vardas" Tomas
 // patikrinti ar veikia f-ja
+ function getVardas () {
+   return "Tadas";
+ }
+var vardas = getVardas ();
+console.log( getVardas ()  );
+
+
+
+
+
+
 
 // 3 UZDUOTIS
 // parasyti f-ja "getVardasPavarde()", kuri turi "return" zodeli ir grazina varda ir pavarde i iskveitimo vieta.
 // sukurti kintamaji   "pavarde" Tomauskas, vardas - Antanas
 // patikrinti ar veikia f-ja
+
+function getVardasPavarde () {
+return "AntanasTomauskas";
+
+}
+var vardasPavarde = getVardasPavarde();
+console.log( getVardasPavarde ()  );
+
+
 
 // 4 UZDUOTIS
 // A) sukurti h1 su tekstu viduje  (HTML faile)
@@ -60,10 +88,34 @@
 // C) su CSS pakeisti h1 auksti, ir paziureti ar jusu f-ja veikia
 
 
+// offsetHeight - elemento aukstis su remeliu iskaiciavus remeli.
+var auks = document.querySelector("body h1").offsetHeight;
+console.log( "h1 aukstis yra:"  + auks);
+document.querySelector("body h1").style.height = '200px';
+document.querySelector("body h1").style.backgroundColor = '#a3a3a3';
+document.querySelector("body h1").style.color = 'red';
+
+// arba
+
+var elementasH1 = document.querySelector("body h1");
+
+
+
+
+
+
+
+
 // 4.1 UZDUOTIS
 // parasyti f-ja, kuri turi "return" zodeli.
 // "getPazymiuVidurkis2(x1, x2, x3, x4, x5)"  f-ja apskaiciuoja vidurki is paduotu 5 menesiu pazymiu (ir grazina atsakyma i iskvietimo vieta! "return atskymas;")
 
+function getPazymiuVidurkis2 (x1, x2, x3, x4, x5) {
+  var  vidurkis = (x1, x2, x3, x4, x5)/5;
+  return vidurkis;
+}
+var birutesvidurkis = getPazymiuVidurkis2(10, 6, 10, 9, 8);
+console.log( "Birutes pazymiu vidurkis:" + birutesvidurkis);
 
 // =================FOR intro  ======================
 
