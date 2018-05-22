2baf74934daf4d930dce1a979ddd607e074a1c30
Tautvydas <arttgd@gmail.com>
2018-05-22 20:24:35 +0300

    8-10 days

---
+++ uzduotys/diena_10/UZDUOTYS/uzduotys--11.2-js--create-element.js
@@ -0,0 +1,42 @@
+
+
+
+
+// 1. sukurti "header"  DOM objekta/NODE'a
+// test
+// console.log(  manoHeader );
+
+// 2. sukurti 'tekstas' kintamaji ir sukurti teksto Node : panaudojant js document.createTextNode(xxx)
+
+
+// 3. i susikuta 'header' objekta idedame 'tekstas' objekta
+// test
+// console.log(  manoHeader );
+
+// 4. headeri idesime po H1
+//        tetis                kaIdeti  PriesKa
+// document.body.insertBefore(  manoAside, manoH1 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
