e3db660daaaf2b27d2ce4fb43f60a35fd68116f3
grajauskas <e.grajauskas@gmail.com>
2018-05-28 12:15:38 +0300

    zinute

---
+++ dirbame_cia/diena_11/DESTYTOJAS/2-php-switch/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>scope</title>
+         <!--  kad ant ivairiu irenginiu media veiktu   -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css">
+        <!-- mano failas visada zemiau -->
+        <link rel="stylesheet" href="css/master.css">
+
+    </head>
+
+    <body>
+        <h1 > Isijunk console </h1>
+
+
+        <?php
+            $kintamasis = 'skorpijonas';
+            switch ($kintamasis) {
+                case 'avinas':
+                        echo "Jusu laukia sekminga diena<br>";
+                    break;
+                case 'jautis':
+                        echo "Jusu laukia nesekme finansuose<br>";
+                    break;
+                case 'vandenis':
+                        echo "Jusu laukia staigmena<br>";
+                    break;
+                default:
+                        echo "tokio \"  $kintamasis \" zodiako neturime <br>";
+                    break;
+            }
+
+            //=====================
+            // koks automobilis?
+            $tekstas = "  Audi";
+
+            // paverciame i mazasias raides, bet nesugadinam orginalaus kintamojo
+            $auto = strtolower( $tekstas);
+            // pasalina tarpus ir tab is abieju sonu (bet ne is teksto vidurio)
+            $auto = trim(  $auto );
+            switch ($auto) {
+                case 'audi':
+                        echo "<h3>puikus pasirinkimas</h3>";
+                    break;
+                case 'bwm':
+                        echo "<h3> greitas psirinkimas </h3>";
+                    break;
+
+                default:
+                        echo "<h3> nieko nezinome spie jusus pasirinkima </h3>";
+                    break;
+            }
+
+         ?>
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <!-- MANO JS failas pats zemiausias -->
+        <script type="text/javascript" src="main.js" >  </script>
+    </body>
+</html>
