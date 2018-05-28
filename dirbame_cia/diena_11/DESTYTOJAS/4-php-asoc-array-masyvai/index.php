5bf52494326dab85268ce0b0f60027dc7b791999
grajauskas <e.grajauskas@gmail.com>
2018-05-28 13:58:38 +0300

    zinute

--- dirbame_cia/diena_11/DESTYTOJAS/4-php-asoc-array-masyvai/index.php
+++ dirbame_cia/diena_11/DESTYTOJAS/4-php-asoc-array-masyvai/index.php
@@ -14,30 +14,15 @@
 
     </head>
 
-    <body class="container">
+    <body>
         <h1 > Isijunk console </h1>
 
-        <?php
-            $kaina = 500;
 
-            echo "<div class='row'>";
-                for ($i=0; $i < 6 ; $i++) {
-                    require_once( "preke.php"); 
-                }
-            echo "</div>";
 
-            // UZDUOTIS:
-            // 1: sukurti "preke.php" faila
-            // h2 (antraste)
-            // p (aprasymas)
-            // button (Kaina)
-            // 2: paleisti FOR cikla 6 kartus (ideti container ir row)
-            // 3) index.php
-            // sukurti kintamuosius:
-            // $kaina = ...;
-            // $antraste = ...;
-            // $aprasymas = ...;
-         ?>
+
+
+
+
 
 
 
--- uzduotys/diena_11/UZDUOTYS/2.2 uzduotis-PHP-inlude-header-footer-preke.php
+++ uzduotys/diena_11/UZDUOTYS/2.2 uzduotis-PHP-inlude-header-footer-preke.php
@@ -9,16 +9,26 @@ main.php
 footer.php
 index.php
 
-Faile index.php naudojant include('kelias'); f-ja "iterpti" header, main ir footer failus
+================turetu gautis======
+
+Faile index.php naudojant include('kelias'); f-ja "iterpti" header, main ir footer failus:
+index.php
+    include('header.php');
+    include('main.php');
+    include('footer.php');
 
 header.php   faile - turetu buti visas puslapio virsus:
  <html>
      <head>
          ...
      </head>
+     <body>
+
+main.php  faile - turetu buti:
+<h1> Mano puslapis su PHP</h1>
 
 footer.php faile - turetu buti visa puslapio apacia:
-<!-- MANO JS failas pats zemiausias -->
+        <!-- MANO JS failas pats zemiausias -->
         <script type="text/javascript" src="main.js" >  </script>
     </body>
 </html>
