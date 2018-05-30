a0961c08a1e09c643881e12019cb21f9460afbf2
gguzelis <g.guzelis@googlemail.com>
2018-05-30 08:36:04 +0300

    update

---
+++ dirbame_cia/diena_12/Gediminas/jqueryeffect/main.js
@@ -0,0 +1,18 @@
+console.log("Labas ");
+
+
+$('h1').hide(3000);
+
+
+$('p').click(function(event) {
+    $(this).hide(1000);
+
+});
+
+$('p').mouseenter(function(event) {
+    /* Act on the event */
+    $(this).css("color", 'pink');
+});
+mouseleave
+
+// $('h1').css('color', 'pink');
