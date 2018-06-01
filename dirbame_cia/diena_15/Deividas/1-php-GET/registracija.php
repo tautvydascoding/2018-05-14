4aa241363a00697cb65cf34d1f5863177325ac4f
Deividas <dvds.zukauskas@gmail.com>
2018-06-01 14:22:40 +0300

    darbai

--- dirbame_cia/diena_15/Deividas/1-php-GET/registracija.php
+++ dirbame_cia/diena_15/Deividas/1-php-GET/registracija.php
@@ -1,31 +1,37 @@
-
 <?php
 
- // echo "aa";
- // print( "aa");
- // print_r("aa");
- // printf("aaa %s", "a11");    seip prisimint
- // var_dump("aaa");
- // die("aa");
+ print_r(  $_GET );
+ print_r(  $_POST );
+ // print_r(  $GLOBALS );
+
+ // $GLOBALS['TAUTVYDAS'] = "slaptazodis";  // i globasa masyva galima isideti betkokia savo reiksme
+
+ $id = $_GET['8'];
+
+ echo "<h2>   $id </h2>";
 
 
-print_r($_GET);
-print_r($_POST);
-//print_r($GLOBALS)
-
- $GLOBALS['TAUTVYDAS'] = "slaptazodis";
- $vartotojoVardas = $_GET['vardas'];
- $vartotojoPavarde = $_GET['pavarde'];
- $vartotojoElPastas = $_GET['elPastas'];
- $vartotojoTel = $_GET['tel.'];
+ ?>
+ 
 
 
 
 
- echo "<h2> $vartotojoVardas $vartotojoPavarde </h2>";
+<?php
+if (true) {
+    echo "<script type='text/javascript'>
+                alert('jau suveikiau');
+          </script>";
+}
 
- if (true) {
-   echo "<script type='text/javascript'>
-      console.log('jau suveikiau');
-   </script>";
- }
+
+// echo "aa";
+// print("aa");
+// print_r("aa");
+// printf("aaa %s" , "a11");
+// var_dump("aaa");
+// die("aa");
+
+
+
+// neuzdarome
