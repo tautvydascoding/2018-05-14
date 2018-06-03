2a27bbfd7c47760dc8916d8b6a94aaa28bf0e5b2
LAPTOP-KSN0G2K5\Ieva Drevinskienė <i.drevinskiene@yahoo.com>
2018-06-03 18:23:38 +0300

    no message

--- dirbame_cia/diena_1/Ieva/Projektas/contacts.php
+++ dirbame_cia/diena_1/Ieva/Projektas/contacts.php
@@ -1,22 +1,32 @@
 
+
+      <?php
+      include 'header.php';
+
+       ?>
+
+
+
+
           <div class="row mt-5">
-              <main class="fonas col-md-5 mt-5 text-justify aukstis-400">
-                <h3 class="mt-5"> Contact Info: </h3>
-                  <p class="p-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p class="p-0"> <strong>Address:</strong> 10111 Santa Monica Boulevard, LA </p>
-                  <p class="p-0"> <strong>Phone:</strong> +44 000 0008 </p>
-                  <p class="p-0"> <strong>E-mail:</strong> info@Example.com </p>
+              <main class=" col-md-5 mt-5 text-justify">
+                <h3 class="textContact mt-5"> Contact Info: </h3>
+                  <p class="textContact p-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p class="textContact p-0"> <strong>Address:</strong> 10111 Santa Monica Boulevard, LA </p>
+                  <p class="textContact p-0"> <strong>Phone:</strong> +44 000 0008 </p>
+                  <p class="textContact p-0"> <strong>E-mail:</strong> info@Example.com </p>
               </main>
 
-              <aside class="fonas col-md-7 mt-5 aukstis-400 d-flex">
-                <form>
+              <aside class="col-md-7 mt-5 d-flex">
+
+                <form class="comment.php" method="get">
                   <div>
-                    <input class="w-100 mb-3" type="text" name="name" placeholder="Your Name">
-                    <input class="w-100 mb-3" type="email" name="name" placeholder="Your Email">
-                    <textarea class="w-100 mb-3" name="text" rows="5" cols="80" placeholder="Your Comment"></textarea>
+                    <input class="w-100 mb-3" type="text" name="name" maxlength="15" placeholder="Your Name" required>
+                    <input class="w-100 mb-3" type="email" name="email" placeholder="Your Email" pattern="a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                    <textarea class="w-100 mb-3" name="text" rows="5" cols="80" maxlength="100" placeholder="Your Comment" pattern="[^'\x22]+" title="Invalid input"></textarea>
                   </div>
 
-                  <button class="w-100" type="button" name="button">Send</button>
+                  <button class="w-100" type="submit" name="button">Send</button>
 
                 </form>
                 </aside>
@@ -25,4 +35,4 @@
           </div>
 <!-- IDEA: užsidaro wrapper dalis -------------------------->
 
-          
+        <?php include'footer.php' ?>
