da24680ff1d1ab73757ad3fee9f144592fc50129
OvidijusKutkaitis <o.kutkaitis@gmail.com>
2018-06-03 21:30:38 +0300

    opapa

---
+++ dirbame_cia/diena_15/Ovidijus/Projektas-Opapa/footeris.php
@@ -0,0 +1,26 @@
+<div class="container-fluid  bg-warning mt-2">
+  <div class="row mb-3">
+    <div class="col-md-4 text-center mt-3 padding">
+      <h2>Kontaktai</h2>
+      <h5>Tel: 860852852</h5>
+      <h5>El.paštas: darbai@darbai.lt</h5>
+      <div class="col-4 social padding">
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <h6 class="text-center ">Iveskite savo duomenis, greitu metu su Jumis susisieksime</h6>
+      <input class="text-center mb-1" type="text" name="vardas" maxlength="15" placeholder="Jūsų vardas"><br />
+      <input class="text-center mb-1" type="email" name="elpastas" placeholder="El.paštas" title="aaaaa@aaaaaa.lt" ><br />
+      <input class="text-center mb-1" type="tel" name="Tel.numeris"  placeholder="861234567" pattern="[0-9]{9}" title="Tel numeris ne maziau 9 simboliu" required><br />
+      <input class="mb-1" type="submit" name="" value="Siųsti">
+    </div>
+
+    <div class="col-md-4">
+      <img src="" alt="LOGO">
+    </div>
+
+  </div>
+</div>
---
+++ dirbame_cia/diena_15/Ovidijus/Projektas-Opapa/paslaugos.php
@@ -0,0 +1,77 @@
+<?php
+include("header.php");
+?>
+<!-- Headerio pabaiga -->
+<body>
+<!-- NAVIGACIJA -->
+<?php
+include("navigacija.php");
+?>
+<!-- Navigacijos pabaiga -->
+<!-- PASLAUGOS -->
+
+<h1 class="text-center">MŪSŲ TEIKIAMOS PASLAUGOS</h1>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididun</p>
+
+<div class="container-fluid">
+  <div class="row">
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+    <div class="col-md-3 col-ms-6">
+      <img src="" alt="">
+      <h6 class="text-center">Paslauga</h6>
+    </div>
+
+  </div>
+</div>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+
+
+
+
+
+<!-- Paslaugos pabaiga -->
+<!-- Footeris pradzia -->
+<?php
+include("footeris.php");
+?>
+<!-- Footerio pabaiga -->
+
+<!--\/ linkai \/  -->
+<?php
+include("footerLink.php");
+ ?>
