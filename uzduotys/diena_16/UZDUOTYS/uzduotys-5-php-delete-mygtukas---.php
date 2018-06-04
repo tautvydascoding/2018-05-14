43771337337ea650903c36dedd5cab5496469472
grajauskas <e.grajauskas@gmail.com>
2018-06-04 12:16:43 +0300

    16 diena

--- uzduotys/diena_16/UZDUOTYS/uzduotys-5-php-delete-mygtukas---.php
+++ uzduotys/diena_16/UZDUOTYS/uzduotys-5-php-delete-mygtukas---.php
@@ -1,3 +1,7 @@
- sukurti delete mygtuka
-ir kai ji paspaudzia, vartotoja perkelti i delete.php faila
-delete.php faile iskviesti savo pasirasyta f-ja  deleteDoctor(1);
+PATOBULINTI 4- uzduoti:
+1. salia gydytoju vardu ideti "Trinti" mygtuka
+
+1.2. Kai mygtuka paspaus vartotojas - vartotoja perkelti i "delete-doctor.php" faila
+galite panaudoti "<a>Trinti</a>"
+
+2.1 delete-doctor.php faile iskviesti savo pasirasyta f-ja  deleteDoctor(1);
