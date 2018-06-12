596c9eefd196a84883196aabff44625762823c74
LAPTOP-KSN0G2K5\Ieva Drevinskienė <i.drevinskiene@yahoo.com>
2018-06-12 14:58:07 +0300

    no message

---
+++ dirbame_cia/diena_17/Ieva/php  class su funkcijomis/zmogus.php
@@ -0,0 +1,15 @@
+
+
+
+      <?php
+
+        //UZDUOTIS 1
+        // SUKURTI KLASE zmogus, zmogus turi private ugis, private vardas, public f-ja "manoUgis()", manoVardas();
+
+
+        class Zmogus {
+            private $vardas = ""; // nurodomas tipas, pvz string
+            private $ugis = 0; // kintamiesiems nurodome public, private, gali buti =-999, galima is viso nedeti lygybes pvz $ugis;
+            public function manoUgis() { echo ""; }
+            public function manoVardas() { echo ""; }
+        }
