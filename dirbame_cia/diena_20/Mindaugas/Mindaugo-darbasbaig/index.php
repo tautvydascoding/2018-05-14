6d3c2e2af59b33c9530db1dcdd41e4328a0ff672
DESKTOP-HL4O3SL\Mindaugas <mindaugas.vidut@gmail.com>
2018-06-12 22:49:28 +0300

    vakaro darbai

--- dirbame_cia/diena_20/Mindaugas/Mindaugo-darbasbaig/index.php
+++ dirbame_cia/diena_20/Mindaugas/Mindaugo-darbasbaig/index.php
@@ -6,7 +6,8 @@
          <!--  kad ant ivairiu irenginiu media veiktu   -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-          <script src="libs/jquery-3.2.1.min.js" type="text/javascript"> </script>
+        <link rel="stylesheet" href="css/normalize.css">
+
         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css">
         <!-- mano failas visada zemiau -->
@@ -17,16 +18,6 @@
     <body>
         <h1 > Isijunk console </h1>
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-8 bg-danger">
-      <div class="col-md-4 bg-info" >
-
-      </div>
-
-    </div>
-  </div>
-</div>
 
 
 
@@ -35,10 +26,31 @@
 
 
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+
+        <h3>My Google Maps Demo 1s</h3>
+            <!--The div element for the map -->
+            <div id="map"></div>
+
+
+
+
+
+
+
+            <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDH75LLjLcOxDy2LcRLisAwIOUm-flMdB0&callback=initMap">
+            </script>
+
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+        <script src="libs/jquery-3.2.1.min.js" type="text/javascript"> </script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
         <!-- MANO JS failas pats zemiausias -->
         <script type="text/javascript" src="main.js" >  </script>
+
+
+
     </body>
 </html>
