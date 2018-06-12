e20c9f6eceefddb1fa14f075f5df3b10e1496287
Tomrial <39274152+Tomrial@users.noreply.github.com>
2018-06-12 11:04:28 +0300

    Jungiu

--- dirbame_cia/diena_7/Tomas/Projekto_pradzia/body.php
+++ dirbame_cia/diena_7/Tomas/Projekto_pradzia/body.php
@@ -1,95 +1,41 @@
-<<<<<<< HEAD:dirbame_cia/diena_7/Tomas/Projekto_pradzia/index.html
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="utf-8">
-    <title>Smalsūs šnekučiai</title>
-    <!--  kad ant ivairiu irenginiu media veiktu   -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Kavoon" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css">
-    <!-- mano failas visada zemiau -->
-    <link rel="stylesheet" href="css/master.css">
-
-</head>
-
-<body>
-<header>
-    Labas
-</header>
-
-    <nav class="container-fluid spalvaNav navbar navbar-expand-sm  mb-2 navbar-dark shadow-lg sticky-top aukstis-100">
-
-
-        <img src="img/logotipas.png" alt="logotipas Smalsus snekuciai">
-
-
-        <ul class="navbar-nav">
-            <li class="nav-item flex">
-                <a class="" href="#">Home</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="" href="#">Apie mus</a>
-            </li>
-            <li class="nav-item">
-                <a class="" href="#">Galerija</a>
-            </li>
-            <li class="nav-item">
-                <a class="" href="#">Kainos </a>
-            </li>
-            <li class="nav-item">
-                <a class="" href="#">Kontaktai </a>
-            </li>
-            <li class="nav-item">
-                <a class="" href="#">Priėmimas</a>
-            </li>
-        </ul>
-
-    </nav>
-
-
-
-
-
-<h1></h1>
 <div class="container">
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner" role="listbox">
-=======
-<h1></h1>
-<div class="container">
-    <div id="carousel" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
->>>>>>> d1bdff441d2acab6001cde19bae895f4cb65a135:dirbame_cia/diena_7/Tomas/Projekto_pradzia/body.php
-    <div class="carousel-item active">
-      <img class="d-block img-fluid" src="img/child.jpg" alt="First slide">
     </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="img/games.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="img/people.jpg" alt="Third slide">
-    </div>
-  </div>
-</div>
-</div>
+    <div class="carousel-inner" role="listbox">
 
-
+    </div>
+    <h1></h1>
     <div class="container">
-      <h3 style="text-align:center">Smalsiuose šnekučiuose rasite:</h3>
-<ul style="text-align:center">
-    <li>Žaidimų erdvę.</li>
-    <li>Mini darželį (valandinį).</li>
-    <li>Įvairių būrelių.</li>
-    <li>Gimtadienio erdvę!</li>
-</ul>
-<div class="container">
-    <img src="img/Capture1.png" class="rounded mx-auto d-block" alt="">
+        <div id="carousel" class="carousel slide fluid" data-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img class="d-block img-fluid" src="img/K1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/K2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid" src="img/K3.jpg" alt="Third slide">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
-    </div>
+
+<h3 style="text-align:center">Smalsiuose šnekučiuose rasite:</h3>
+<div class="">
+
+    <ul style="text-align:center">
+        <li>Žaidimų erdvę.</li>
+        <li>Mini darželį (valandinį).</li>
+        <li>Įvairių būrelių.</li>
+        <li>Gimtadienio erdvę!</li>
+    </ul>
+</div>
+<div class="">
+    <img src="img/Capture1.png" class="mb-3 dydis2 rounded mx-auto d-block " alt="">
+</div>
