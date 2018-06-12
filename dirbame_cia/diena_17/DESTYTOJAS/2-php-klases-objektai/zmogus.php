597a84893ace4bef9738d7752b009e5f263009f0
grajauskas <e.grajauskas@gmail.com>
2018-06-12 14:49:41 +0300

    18diena

---
+++ dirbame_cia/diena_17/DESTYTOJAS/2-php-klases-objektai/zmogus.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+class Zmogus {
+    public $vardas = "nevarde";   // string
+    public $ugis = -999;
+
+    public function einu() {
+        echo "einu einu <br />";
+    }
+}
+//
+
+
+//
