9ea2098f0d64aa73ecbb8e9e7daec360c4dd8636
OvidijusKutkaitis <o.kutkaitis@gmail.com>
2018-06-12 08:34:21 +0300

    opapa

--- dirbame_cia/diena_15/Ovidijus/Projektas-Opapa/kontaktai.php
+++ dirbame_cia/diena_15/Ovidijus/Projektas-Opapa/kontaktai.php
@@ -10,7 +10,7 @@ include("navigacija.php");
 <!-- Navigacijos pabaiga -->
 <!-- KONTAKTAI -->
 <div class="container">
-  <div class="row mt-2 ">
+  <div class="row mb-2 ">
     <div class=" col-md-5 col-ms-12 kontaktai">
       <h2 class="text-center">MŪSŲ KONTAKTAI</h2><br />
         <h5 class="text-center">LIETUVA, KAUNAS</h5>
@@ -29,6 +29,12 @@ include("navigacija.php");
     </div>
 
   </div>
+
+  <section class="container" id="Map">
+    <div class="row col-md-12 col-sm-12">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d146862.78023452358!2d23.78717946565383!3d54.8900919008326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e72270b167d343%3A0x614bd1e0c7378bbc!2sKaunas!5e0!3m2!1slt!2slt!4v1528698540226" width="1200" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+  </section>
 
 
 <!-- Kontaktai pabaiga -->
