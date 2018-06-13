0e2c4713182af75b64c3a76d5b7fc1545b484df0
grajauskas <e.grajauskas@gmail.com>
2018-06-13 10:24:48 +0300

    diena 18.1

--- uzduotys/diena_18/UZDUOTYS/uzduotys-1-PHP--pasikartoti-private-public.php
+++ uzduotys/diena_18/UZDUOTYS/uzduotys-1-PHP--pasikartoti-private-public.php
@@ -8,13 +8,9 @@
  // "A" klase turi kintamuosius:
  // name , plaukuSpalva, sirdiesYda
  // kurie yra "public", "private", "protected"
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> d3adfae6f1a353ce8bca22447d9d2057193bee38
  // 2. SUSIKURTI "B" objekta
- // 3. atspausdinti visus kintamuosius (nenaudojant/nesikurti "A" objekto IR nenaudojant "echo" komandos "A" kalseje)
+ // 3. atspausdinti visus kintamuosius (nenaudojant/nesikurti "A" objekto IR nenaudojant "echo" komandos "A" klaseje)
 
 
  //
