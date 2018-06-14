05c1b9d1246844dfaf6d6aafd3d16334770bd0ad
Tomrial <39274152+Tomrial@users.noreply.github.com>
2018-06-14 08:43:24 +0300

    Jungiu

--- dirbame_cia/diena_7/Tomas/Projekto_pradzia/Kainos.php
+++ dirbame_cia/diena_7/Tomas/Projekto_pradzia/Kainos.php
@@ -34,3 +34,59 @@ if ($kaina1 != NULL){
 <div class="container">
 <p>Valandos nuo 9 iki 18 val. Esant poreikui galime atsidaryti anksciau ir darba baigti veliau. Pries atvykstant prasome labai prasome parasyti mums zinute (i FB) arba paskambinti (bent valandele pries atvykima).</p>
 </div>
+<!-- <form   action="email.php" method="post">
+    <input type="text" name="vardas"  placeholder="vardas" >
+    <input type="email" name="pastas" placeholder="el. pastas"   required>
+    <textarea name="klausimas" rows="8" cols="80" placeholder="Jusu klausimas"  required>  </textarea>
+    <button type="submit" name="button" >
+        Klausti
+    </button>
+</form> -->
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<div class="container">
+    <br/>
+    <div class="abc">
+        <div class="panel-body">
+            <h2 style="color:#ffb90f"><b> <center>Susisiekite su mumis</center> </b></h2></div>
+    </div>
+</div>
+<br/>
+<br/>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8" style="background-color:#00cdcd;">
+            <div class="well well-sm" style="background-color:#00cdcd;">
+                <form action="email.php" method="post" style="background-color:#00cdcd;">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">
+                                  Vardas</label>
+                                <input type="text" class="form-control" name="vardas" id="name" placeholder="Jūsų vardas" required="required" />
+                            </div>
+                            <div class="form-group">
+                                <label for="email">
+                                  El. Paštas</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                    </span>
+                                    <input type="email" class="form-control" name="pastas" id="email" placeholder="Įrašykite el. pašto adresą" required="required" /></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">
+                                  Žinutė</label>
+                                <textarea name="klausimas" id="message" class="form-control" rows="5" cols="25" required="required" placeholder=" Jūsų žinutė"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+                              Siųskite žinutę</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
