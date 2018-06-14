52ae42aa5c5f6a7adc760098dc184b44732a34fb
grajauskas <e.grajauskas@gmail.com>
2018-06-14 15:07:31 +0300

    aa

---
+++ dirbame_cia/diena_19/DESTYTOJAS/3-php-abstract-class/edite.php
@@ -0,0 +1,16 @@
+<?php
+// uzduotis:
+// 1) sukurti interface "Edit" :
+//  1.1) kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
+    // changeName($a)
+    // getName()
+    // isLogin()
+    // NOTE: Ar interface gali tureti kintamuosius????
+
+
+interface Edit  {
+    public function changeName($a);
+    public function getName();
+    public function isLogin();
+}
+ 
